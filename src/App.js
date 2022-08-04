f777a11e877528857d6aca981600a524aec188f7
Dipankar <dipankar15-11137@diu.edu.bd>
2022-08-05 00:41:09 +0600

    footer add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
+import Home from './Components/Home/Home'
+import Navbar from './Components/Share/Navbar'
+import Footer from './Components/Share/Footer'
 
 function App() {
   return (
     <div >
-
+      <Navbar></Navbar>
+      <Home></Home>
+      <Footer></Footer>
     </div>
   );
 }
